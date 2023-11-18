577385b1b48209038f2da8500486d96c699823df
JBisquick <felipebirch@gmail.com>
2023-11-18 14:09:11 -0800

    Add a way to switch between pages

--- src/index.js
+++ src/index.js
@@ -2,6 +2,25 @@ import loadStartPage from './loadPages/loadStartPage';
 import loadMenu from './loadPages/loadMenu';
 import loadHome from './loadPages/loadHome';
 import loadContact from './loadPages/loadContact';
+import removeContainer from './loadPages/removeContainer';
 
 loadStartPage();
 
+const home = document.getElementById('home');
+const menu = document.getElementById('menu');
+const contact = document.getElementById('contact');
+
+home.addEventListener('click', function() {
+  removeContainer();
+  loadHome();
+})
+
+menu.addEventListener('click', function() {
+  removeContainer();
+  loadMenu();
+})
+
+contact.addEventListener('click', function() {
+  removeContainer();
+  loadContact();
+})
